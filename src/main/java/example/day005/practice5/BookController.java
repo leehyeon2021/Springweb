4b7e704107b7bed1df37c9b6492fdf2837b06753
leehyeon2021 <leehyeon2021@gmail.com>
2026-03-04 18:16:16 +0900

    260304 오후

--- src/main/java/example/day005/practice5/BookController.java
+++ src/main/java/example/day005/practice5/BookController.java
@@ -1,6 +1,5 @@
 package example.day005.practice5;
 
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/example/day005/practice5/BookEntity.java
+++ src/main/java/example/day005/practice5/BookEntity.java
@@ -23,4 +23,14 @@ public class BookEntity {
     @Column( name = "bpub" , length = 50 , nullable = true)
     private String bpub;
 
+    /*
+    mysql(db): 대소문자 구별 안 함
+    java: 대소문자 구별 함.
+
+    JPA에서는 Entity의 속성명과 mysql 테이블의 속성명이 일치해야 한다.
+    Entity에서 bookId 하면 ----> mysql에서 book_id로 매핑됨.
+    - 방법 두 개
+        1. 자바에서 카멜 사용시 mysql테이블 속성명을 _언더바 사용한다.
+        2. 자바에서 카멜을 사용 안 함.
+     */
 }
